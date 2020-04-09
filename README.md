b5d839b8cdb39b7ecce28bb0208ecbbb5d51a611
Thiago Nascimento <tnascimento@tech6.com.br>
2020-04-09 15:26:25 -0300

    update readme data

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Marvel Heroes List
+
+This is a simple project using [MARVEL API](https://developer.marvel.com/docs).
+
+## Dependencies
+
+To enable you to use this project, please create a .env file on root of project and set 
+```javascript 
+    REACT_APP_PUBLIC_MARVEL_KEY=your_key
+```
+
+To get an api key, please visit [MARVEL API](https://developer.marvel.com/docs) an access *Get A Key*.
+
+## Packages 
+
+* [antd](https://ant.design/) - A design system for enterprise-level products. Create an efficient and enjoyable work experience.
+* [axios](https://github.com/axios/axios) - Promise based HTTP client for the browser and node.js
+* [react-redux](https://react-redux.js.org/) - Official React bindings for Redux
+* [react-router-dom](https://reacttraining.com/react-router/) - React Router is a collection of navigational components that compose declaratively with your application.
+* [redux](https://redux.js.org/) - A Predictable State Container for JS Apps
+* [redux-saga](https://github.com/redux-saga/redux-saga) - Is a library that aims to make application side effects easier to manage.
+* [redux-thunk](https://github.com/reduxjs/redux-thunk) - Thunk middleware for Redux.
+* [typesafe-actions](https://github.com/piotrwitek/typesafe-actions) - Typesafe utilities designed to reduce types verbosity and complexity in Redux Architecture.
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -13,9 +13,9 @@ const AboutComponent = () => {
     const dataSource: InstalledPackages[] = [
         { name: 'antd', description: 'A design system for enterprise-level products. Create an efficient and enjoyable work experience.', url: 'https://ant.design/' },
         { name: 'axios', description: 'Promise based HTTP client for the browser and node.js', url: 'https://github.com/axios/axios'},
-        { name: 'react-redux', description: 'Official React bindings for Redux', url: 'https://react-redux.js.org/'},
+        { name: 'react-redux', description: 'Official React bindings for Redux.', url: 'https://react-redux.js.org/'},
         { name: 'react-router-dom', description: 'React Router is a collection of navigational components that compose declaratively with your application.', url: 'https://reacttraining.com/react-router/'},
-        { name: 'redux', description: 'A Predictable State Container for JS Apps', url: 'https://redux.js.org/'},
+        { name: 'redux', description: 'A Predictable State Container for JS Apps.', url: 'https://redux.js.org/'},
         { name: 'redux-saga', description: 'Is a library that aims to make application side effects easier to manage.', url: 'https://github.com/redux-saga/redux-saga'},
         { name: 'redux-thunk', description: 'Thunk middleware for Redux.', url: 'https://github.com/reduxjs/redux-thunk'},
         { name: 'typesafe-actions', description: 'Typesafe utilities designed to reduce types verbosity and complexity in Redux Architecture.', url: 'https://github.com/piotrwitek/typesafe-actions'},
